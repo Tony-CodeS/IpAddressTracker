b170c15e2852c89e704e8bb2490ca4f40e761a97
Tony <asolomon484@gmail.com>
2023-06-09 23:53:47 +0100

    IP Address Tracker

--- app/page.js
+++ app/page.js
@@ -1,113 +1,94 @@
+'use client'
 import Image from 'next/image'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
+import {FaGreaterThan} from 'react-icons/fa'
+import { useState } from 'react';
 
+const apiKey = 'at_Z3OszzOzt2IEtfWGVn10DiELsX2K2'
 export default function Home() {
+ const [formData, setFormData] = useState({ ipAddress: '', data: null });
+  const [mapCenter, setMapCenter] = useState([0, 0]);
+
+  const handleInputChange = (event) => {
+    setFormData({ ...formData, ipAddress: event.target.value });
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    // Make an HTTP GET request to the API endpoint
+    const apiUrl = `https://geo.ipify.org/api/v1?apiKey=${apiKey}&ipAddress=${formData.ipAddress}`;
+
+    // Make the request
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the API response
+        setFormData({ ipAddress: formData.ipAddress, data });
+        setMapCenter([lat, lng]);
+      })
+
+      
+      .catch((error) => {
+        // Handle any errors that occurred during the request
+        console.error(error);
+      });
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className=''>
+    <div className=' bg-red-500 h-56 position-relative'
+       style={{
+        backgroundImage: `url('/pattern-bg-desktop.png')`,
+        }}
+    >
+     <p className='text-center font-medium text-3xl text-white p-4'> IP Address Tracker</p>
+    <div className='flex justify-center items-center'>
+    <form onSubmit={handleFormSubmit}
+    className=' w-11/12 md:w-6/12 lg:w-4/12 mt-6 mb-6 '>
+        <input type='text'
+         placeholder='search for any IP address or domain' 
+        value={formData.ipAddress}
+          onChange={handleInputChange}
+         className='  h-fit w-11/12 p-3 rounded-l-xl border-none outline-0'/>
+        <button className='bg-black w-1/12 p-3  h-12  rounded-r-xl text-white'><FaGreaterThan size={10}/></button>
+      </form>
+     
+    </div>
+    
+       <div className='absolute w-3/4 bg-stone-50 lg:h-40 z-20 ml-12 md:ml-28 lg:ml-36 flex flex-col md:flex-row lg:flex-row justify-center items-center rounded-lg text-center'>
+       <div className='w-full lg:w-1/4  lg:border-solid border-1 border-black h-20 lg:h-16'> 
+       <p className='text-gray-400 font-medium'>IP Address</p>
+       <p className='text-xl font-bold'>{formData.data ? formData.data.ip:'' }</p>
+       </div>
+       <div className='w-full lg:w-1/4 border-t border-black lg:border-solid lg:border-x-2 lg:border-t-0 h-20  lg:h-16 '>
+        <p  className=' text-gray-400 font-medium'>Region</p>
+        <p className='text-xl font-bold'>{formData.data ? formData.data.location.region: ''}</p>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+       <div className='w-full border-t lg:border-none lg:w-1/4  border-black h-20   lg:h-16'>
+        <p  className='text-gray-400 font-medium'>Country</p>
+        <p className='text-xl font-bold'>{formData.data ? formData.data.location.country: ''}</p>
+        </div>
+       <div className='w-full border-t lg:w-1/4   lg:border-l-2 lg:border-t-0 border-black  h-20  lg:h-16'>
+        <p  className='text-gray-400 font-medium'>Timezone</p>
+      <p className='text-xl font-bold'>{formData.data ? formData.data.location.timezone: ''}</p>
+        </div>
+     </div>
+  
+   
+    </div>
+    
+      {formData.data && formData.data.location && (
+        <MapContainer center={[formData.data.location.lat, formData.data.location.lng]} zoom={13} scrollWheelZoom={false} className='h-screen z-10'>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[formData.data.location.lat, formData.data.location.lng]}>
+            <Popup>{formData.data.location.city}</Popup>
+          </Marker>
+        </MapContainer>
+      )}
     </main>
   )
 }
